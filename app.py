3ef3662e385e59bcd8bac785d7ad567dc7bfae83
MrS4nd <changyifong@outlook.com>
2024-03-06 00:24:07 +0700

    initial

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import random
+import itertools
 
-st.header("Let's pick what you want to eat 😋")
+st.header("Let's random what you want to eat 😋")
 
 drinks = 'drinks.txt'
 
@@ -10,6 +11,7 @@ viet = 'viet.txt'
 jap = 'japan.txt'
 ko = 'korea.txt'
 ch = 'ch.txt'
+
 
 def drink_menu(drinks):
     with open(drinks,'r') as d:
@@ -47,6 +49,12 @@ def China_menu(ch):
         china_lines = random.choice(c_lines)
     return china_lines.strip()
 
+user = st.radio('Do you have any preferred foods that you\'d like?',options = ['Yes', 'No'],horizontal=True)
+if user == 'Yes': 
+    user_input = st.text_input("Your food option: ")
+    food_op = [option.strip() for option in user_input.split(",")]
+    random_user = random.choice(food_op)
+
 al = st.radio('Are there any foods that you allergies for?',options = ['Yes', 'No',],horizontal=True)
 if al == 'Yes':
     allergy = st.multiselect('What types of food are you allergic to?',['Dairy products 🥛','Eggs 🥚','Vegetables 🥗','Fish 🐟','Crustacean shellfish 🦐',
@@ -56,29 +64,38 @@ pick = st.radio('Are there any foods that you don\'t eat?',options = ['Yes', 'No
 if pick == 'Yes':
     choose = st.multiselect('Are there any foods that you don\'t eat?',['Pork 🐖','Beef 🐄','Chicken 🐓','Spicy 🌶️','Seafood 🦪'])
 
-nation = st.multiselect('Which cuisine\'s dishes do you prefer?',['Thailand 🛕','Vietnam 🪷','Japan ⛩️','Korea 🫰','China 🧧','Other 😶'])
+nation = st.multiselect('Which cuisine\'s dishes do you prefer?',['Thailand 🛕','Vietnam 🪷','Japan ⛩️','Korea 🫰','China 🧧'])
 
 drink = st.radio('Would you like a drink with that?',options = ['Yes','No'],horizontal=True)
 
-if st.button('Start Random') and nation:
-    
-    if 'Thailand 🛕' in nation:
-        st.write("For Thai dishes :", thai_menu(th))    
+if st.button('Start Random') or nation:
+    if user == 'Yes':
+        st.write("You should eat :", random_user)
+
+    if nation == True:
+
+        st.write("")
+
+        if 'Thailand 🛕' in nation:
+            st.write("For Thai dishes :", thai_menu(th))    
         
-    if 'Korea 🫰' in nation:
-        st.write("For Korea dishes :",Korea_menu(ko))
+        if 'Korea 🫰' in nation:
+            st.write("For Korea dishes :",Korea_menu(ko))
     
-    if 'Vietnam 🪷' in nation:
-        st.write("For Vietnam dishes :",Viet_menu(viet))
+        if 'Vietnam 🪷' in nation:
+            st.write("For Vietnam dishes :",Viet_menu(viet))
     
-    if 'Japan ⛩️' in nation:
-        st.write("For Japan dishes :",Japan_menu(jap))
+        if 'Japan ⛩️' in nation:
+            st.write("For Japan dishes :",Japan_menu(jap))
 
-    if 'China 🧧' in nation:
-        st.write("For Chinese dishes :",China_menu(ch))
+        if 'China 🧧' in nation:
+            st.write("For Chinese dishes :",China_menu(ch))
 
-    if drink == 'Yes':
-        st.write("For drinks :", drink_menu(drinks))
-    
+        if drink == 'Yes':
+                st.write("For drinks :", drink_menu(drinks))
+        elif nation == False:
+
+            st.write("Please slect \"Cuisines\" or \"Enter your food options\"")
+
 else:
-    st.write('Please select \"Cuisines\" or press \"Start Random\" 🥺')
+    st.write('Please select \"Cuisines\" or \"Enter your food options\" and then press \"Start Random\" 🥺')
